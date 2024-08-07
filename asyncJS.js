385c4f9c704a3a8351dce5901529aec577a192d8
Mohit Aggarwal <maggarwal011998@gmail.com>
2024-08-08 02:08:50 +0530

    async and await update

--- asyncJS.js
+++ asyncJS.js
@@ -1,54 +1,88 @@
-//set timeout 
+// //set timeout 
 
-function greet(name)
-{
-    console.log(`hello ${name}`)
+// function greet(name)
+// {
+//     console.log(`hello ${name}`)
+// }
+// const result=setTimeout(greet,2000,'Mohit')
+
+// console.log("Post settimeout")
+
+// // clearTimeout(result) to clear set timeout
+
+
+// //callback
+// function greetMohit(greetfn){
+//     const name="Mohit Aggarwal"
+//     greetfn(name)
+// }
+
+// greetMohit(greet)
+
+// /* Promises */
+// const onFullfilled=(params)=>{
+//     console.log(`${params}.Set up the table`)
+// }
+
+// const onRejected=(params)=>{
+//     console.log(`${params}.Start cooking pasta`)
+// }
+
+// const promises = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('can get tacos')
+//     }, 3000),
+//         setTimeout(() => {
+//             reject('cannot get tacos')
+//         }, 2000)
+// }).then(onFullfilled)
+//     .catch(onRejected)
+
+// /*this approach will throw the error.
+
+//  promises.then(onFullfilled)
+// promises.catch(onRejected) */
+
+// const promise1= Promise.resolve(33);
+// const promise2=100;
+// const promise3=new Promise((resolve,reject)=>{
+//     setTimeout(resolve,1000,"promise 3 reolve")
+// });
+
+// Promise.all([promise1,promise2,promise3]).then((result)=>{
+//     console.log(result)
+// })
+
+/* Async and Await */
+async function asynGreet(){
+    return "hello from async greet"
 }
-const result=setTimeout(greet,2000,'Mohit')
 
-console.log("Post settimeout")
+asynGreet() // this will return `Promise {<fulfilled>: 'hello from async greet'}` in browser console.
 
-// clearTimeout(result) to clear set timeout
-
-
-//callback
-function greetMohit(greetfn){
-    const name="Mohit Aggarwal"
-    greetfn(name)
+async function asynGreet02(){
+    return Promise.resolve("hello from async greet 02")
 }
 
-greetMohit(greet)
+asynGreet02() // this will return `Promise {<fulfilled>: 'hello from async greet'}` in browser console.
 
-/* Promises */
-const onFullfilled=(params)=>{
-    console.log(`${params}.Set up the table`)
+async function asynGreetThen(){
+    return "hello from async greet with then keyword"
 }
 
-const onRejected=(params)=>{
-    console.log(`${params}.Start cooking pasta`)
-}
+asynGreet().then(value=>console.log(value)) // this will return `hello from async greet` in browser console.
 
-const promises = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('can get tacos')
-    }, 3000),
-        setTimeout(() => {
-            reject('cannot get tacos')
-        }, 2000)
-}).then(onFullfilled)
-    .catch(onRejected)
+ async function asyncAndAwaitGreet()
+ {
+    let promise = new Promise((resolve,reject)=>{
+        setTimeout(() => resolve("Hello"),3000)
+    });
 
-/*this approach will throw the error.
+    console.log("pre promise")
 
- promises.then(onFullfilled)
-promises.catch(onRejected) */
+    console.log(await promise);
+    
+    console.log("post promise")
+ }
 
-const promise1= Promise.resolve(33);
-const promise2=100;
-const promise3=new Promise((resolve,reject)=>{
-    setTimeout(resolve,1000,"promise 3 reolve")
-});
-
-Promise.all([promise1,promise2,promise3]).then((result)=>{
-    console.log(result)
-})
+ asyncAndAwaitGreet();
